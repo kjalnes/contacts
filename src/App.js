c56f47a23cc0914dbad0f69c59eb622b7ad01f39
kjalnes <kris.alnes@gmail.com>
2017-09-18 16:58:56 -0400

    section 5 complete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI.js';
 
 class App extends Component {
-  state = {
-    screen: 'list',
-    contacts: []
-  }
+    state = {
+        contacts: []
+    }
 
-  removeContact = (contact) => {
-    this.setState( (prevState) => ({
-        contacts: prevState.contacts.filter( c => c.id !== contact.id)
-    }));
+    removeContact = (contact) => {
+        this.setState( (prevState) => ({
+            contacts: prevState.contacts.filter( c => c.id !== contact.id)
+        }));
+        ContactsAPI.remove(contact);
+    }
 
-    ContactsAPI.remove(contact);
-  }
+    componentDidMount() {
+        ContactsAPI.getAll().then(contacts => {
+            return this.setState({contacts})
+        })
+    }
 
-  componentDidMount() {
-    ContactsAPI.getAll().then(contacts => {
-      return this.setState({contacts})
-    })
-  }
+    createContact(contact) {
+        ContactsAPI.create(contact)
+            .then( contact => {
+                this.setState( state => ({
+                    contacts: state.contacts.concat([contact])
+            }))
+        })
+    }
 
-
-  render() {
-    return (
-      <div>
-      { this.state.screen === 'list' && (
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={ () =>{
-            this.setState({ screen: 'create' })}
-          }
-        />
-      )}
-      { this.state.screen === 'create' && (
-        <CreateContact />
-      )}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='app'>
+                <Route exact path='/' render={ () => (
+                    <ListContacts
+                        contacts={this.state.contacts}
+                        onDeleteContact={this.removeContact}
+                    />
+                )}/>
+                <Route path='/create' render={ ( {history} ) =>
+                    <CreateContact
+                        onCreateContact={ (contact) => {
+                            this.createContact(contact)
+                            history.push('/');
+                        }}
+                    />
+                }/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegex from 'escape-string-regexp';
 import sortBy from 'sort-by';
@@ -49,11 +50,10 @@ class ListContacts extends Component {
                         value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
-                    <a
-                    href='#create'
-                    className='add-contact'
-                    onClick={ () => this.props.onNavigate()}
-                    >Add contact</a>
+                    <Link
+                        to='/create'
+                        className='add-contact'
+                    >Add contact</Link>
                     </div>
                 <ol>
                     {showingContacts.map( contact => (
